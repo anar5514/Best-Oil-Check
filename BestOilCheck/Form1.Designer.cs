2cac91b0626de4049ccac1896d4201decd29c470
Anar Axmed <anar.axmed5514@gmail.com>
2019-01-23 16:39:34 +0400

    a

--- BestOilCheck/Form1.Designer.cs
+++ BestOilCheck/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BestOilCheck
 {
-    partial class Form1
+    partial class BestOilCheck
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 7);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(467, 407);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // BestOilCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(472, 411);
+            this.Controls.Add(this.listBox1);
+            this.Name = "BestOilCheck";
+            this.Text = "Best Oil Check";
+            this.Load += new System.EventHandler(this.BestOilCheck_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- BestOilCheck/Form1.cs
+++ BestOilCheck/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,36 @@ using System.Windows.Forms;
 
 namespace BestOilCheck
 {
-    public partial class Form1 : Form
+    public partial class BestOilCheck : Form
     {
-        public Form1()
+        public BestOilCheck()
         {
             InitializeComponent();
+        }
+
+        private void BestOilCheck_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop, false) as string[];
+
+            foreach (var file in files)
+            {
+                MessageBox.Show(file);
+                listBox1.Items.Add(file);
+            }
+
+            //string filee = e.Data.GetData(DataFormats.)
+
+            //JsonConvert.DeserializeObject();
         }
     }
 }

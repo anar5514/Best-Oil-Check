0a5dc763b2016cbcc8ab680c46bdd47da4cf342b
Anar Axmed <anar.axmed5514@gmail.com>
2019-02-04 17:36:22 +0400

    Last

--- BestOilCheck/Form1.cs
+++ BestOilCheck/Form1.cs
@@ -1,9 +1,12 @@
 ﻿using Newtonsoft.Json;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,22 +28,35 @@ namespace BestOilCheck
 
         private void listBox1_DragEnter(object sender, DragEventArgs e)
         {
-            e.Effect = DragDropEffects.All;
+            if(e.Data.GetDataPresent(DataFormats.FileDrop, false))
+                e.Effect = DragDropEffects.All;
         }
 
         private void listBox1_DragDrop(object sender, DragEventArgs e)
         {
-            string[] files = e.Data.GetData(DataFormats.FileDrop, false) as string[];
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
 
             foreach (var file in files)
             {
-                MessageBox.Show(file);
+                MessageBox.Show("You Dropped" + file);
                 listBox1.Items.Add(file);
             }
 
-            //string filee = e.Data.GetData(DataFormats.)
+            PdfDocument pdfDocument = new PdfDocument();
+            pdfDocument.Info.Title = "Created by PDFsharp";
 
-            //JsonConvert.DeserializeObject();
+            PdfPage pdfPage = new PdfPage();
+            XGraphics xGraphics = XGraphics.FromPdfPage(pdfPage);
+            XFont xFont = new XFont("Times New Roman", 10, XFontStyle.Bold);
+
+            xGraphics.DrawString($"{File.ReadAllText(listBox1.Items[0] as string)}", xFont , 
+                XBrushes.Black, new XRect(0,0, pdfPage.Width, pdfPage.Height), XStringFormats.Center);
+            pdfDocument.AddPage(pdfPage);
+
+            pdfDocument.AddPage(pdfPage);
+            pdfDocument.Save(@"C:/Users/Anar/Desktop");
+
+
         }
     }
 }
